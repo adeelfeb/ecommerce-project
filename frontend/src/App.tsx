b1cf228fd72d096470d36284aaac1acf5e1dca59
adeelfeb <at4563323@gmail.com>
2025-04-04 23:24:24 +0500

    complete the setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <head>Amzaon Clone</head>
+     <main>
+      <p>Temp website has been loaded</p>
+     </main>
+     <footer>Copy Right reserved</footer>
+    </>
+  )
+}
+
+export default App
