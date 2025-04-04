1ce8a270080f43d61425aa9b89ba2e570709d47c
adeelfeb <at4563323@gmail.com>
2025-04-05 00:48:23 +0500

    perform basic setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,29 @@
-import { useState } from 'react'
+
+
 import './App.css'
+import { sampleProducts } from './data'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
      <head>Amzaon Clone</head>
      <main>
       <p>Temp website has been loaded</p>
+      <img src="https://placehold.co/600x400" alt="product-image" className="product-image" />
+
+      <ul>
+      {sampleProducts.map(product=> (
+        <li key={product.slug}>
+          <h2>{product.name}</h2>
+          <h3>Price: ${product.price} </h3>
+          <img src={product.image} alt="product-image" className='product-image' />
+          
+
+        </li>
+        ))}
+      </ul>
      </main>
      <footer>Copy Right reserved</footer>
     </>
