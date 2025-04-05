3b3aa440be727c4c7dc5f1c0839d3a092ac66a9e
adeelfeb <at4563323@gmail.com>
2025-04-05 20:58:33 +0500

    use bootstrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,51 @@
+import { useEffect } from 'react';
+import { sampleProducts } from './data';
 
-
-import './App.css'
-import { sampleProducts } from './data'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Col, Container, Nav, Navbar, Row } from 'react-bootstrap';
 
 function App() {
-  
+  useEffect(() => {
+    document.title = "Amazon Clone";
+  }, []);
 
   return (
-    <>
-     <head>Amzaon Clone</head>
-     <main>
-      <p>Temp website has been loaded</p>
-      <img src="https://placehold.co/600x400" alt="product-image" className="product-image" />
-
-      <ul>
-      {sampleProducts.map(product=> (
-        <li key={product.slug}>
-          <h2>{product.name}</h2>
-          <h3>Price: ${product.price} </h3>
-          <img src={product.image} alt="product-image" className='product-image' />
-          
-
-        </li>
-        ))}
-      </ul>
-     </main>
-     <footer>Copy Right reserved</footer>
-    </>
-  )
+    <div className='d-flex flex-column vh-100'>
+      <header>
+        <Navbar bg='dark' variant='dark' expand='lg'>
+          <Container>
+            <Navbar.Brand>T-Commerce</Navbar.Brand>
+          </Container>
+          <Nav>
+            <a href='/cart' className='nav-link'> Cart</a>
+            <a href='/signin' className='nav-link'>Sign In</a>
+          </Nav>
+        </Navbar>
+      </header>
+      <main>
+        <Container className='mt-4 text-center '>
+        <p>Temp Products</p>
+        <Row>
+          {sampleProducts.map(product => (
+            <Col key={product.slug} sm={6} md={4} lg={3} >
+              <img src={product.image} alt="product-image" className="product-image" />
+              <h4>{product.name}</h4>
+              <h6>Price: ${product.price} </h6>
+              
+            </Col>
+          ))}
+        </Row>
+        </Container>
+        
+      </main>
+      <footer>
+        <div className='text-center '>
+        Copy Right reserved
+        </div>
+      </footer>
+    </div>
+  );
 }
+
 
 export default App
